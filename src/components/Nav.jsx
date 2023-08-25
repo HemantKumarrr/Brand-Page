9b9b67879e7b463166e75a93ec975cdb70b391bb
Hemant <hemantkumar29052001@gmail.com>
2023-08-25 22:01:54 +0530

    responsive: final commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,15 +3,29 @@ const Nav = () => {
     <>
       <div className="main">
         <div className="navbar">
-          <img src="./images/brand_logo.png" alt="brand-logo" className="logo" />
+          <img
+            src="./images/brand_logo.png"
+            alt="brand-logo"
+            className="logo"
+          />
           <ul className="nav-items">
-            <li><a href="#">Menu</a></li>
-            <li><a href="#">Location</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
+            <li>
+              <a href="#">Menu</a>
+            </li>
+            <li>
+              <a href="#">Location</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
           </ul>
           <button className="login-btn">Login</button>
+          <i class="ri-menu-line"></i>
         </div>
+        
       </div>
     </>
   );
